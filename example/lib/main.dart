d1e34a6527e3cfad7f9a93c7976bb627642d2512
Wayaer <wayaer@foxmail.com>
2021-09-16 12:56:14 +0800

    init

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('FlJPush with Android')),
-        body: Center(child: Text('集成厂商通道')));
+        body: const Center(child: Text('集成厂商通道')));
   }
 }
